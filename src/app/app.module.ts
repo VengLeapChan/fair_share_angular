440f289a6b92fce9923aac50645edfb6a69c07f8
VengLeapChan <leapvchan@gmail.com>
2024-02-25 23:38:04 -0800

    first mesasge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FairShareProxyService } from './fair-share-proxy.service';
+import { AllReceiptsPageComponent } from './all-receipts-page/all-receipts-page.component';
+import { SingleReceiptPageComponent } from './single-receipt-page/single-receipt-page.component';
+import { CreateNewReceiptPageComponent } from './create-new-receipt-page/create-new-receipt-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllReceiptsPageComponent,
+    SingleReceiptPageComponent,
+    CreateNewReceiptPageComponent,
+    WelcomePageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [FairShareProxyService, provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
